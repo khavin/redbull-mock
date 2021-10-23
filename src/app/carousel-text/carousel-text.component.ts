552a2be57bbdd14740686337fb28bc5fe72d2a32
Khavin K K <khavinkrishnan@gmail.com>
2021-10-23 21:25:05 +0530

    adding transition to story elements

--- src/app/carousel-text/carousel-text.component.ts
+++ src/app/carousel-text/carousel-text.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { timer, Subscription } from 'rxjs';
+
+interface storyListItem {
+  category: string;
+  title: string;
+}
 
 @Component({
   selector: 'app-carousel-text',
@@ -6,17 +12,49 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./carousel-text.component.scss'],
 })
 export class CarouselTextComponent implements OnInit {
-  storyList: Array<string> = [];
+  storyList: Array<storyListItem> = [];
+  currentStory: number = 0;
+  totalStoryCount: number = 5;
+  storyChangetimer = timer(3000, 3500);
+  rxjsSubscriptions: Array<Subscription> = [];
 
   constructor() {}
 
   ngOnInit(): void {
     this.storyList = [
-      'Free Practice Update – United States Grand Prix 2021',
-      'F1 Esports Pro Tips: Driving In The Wet',
-      'Quiz: US Special',
-      'Acura Returns For US Grand Prix',
-      'United States Pre-Race Programme',
+      {
+        category: 'F1',
+        title: 'Free Practice Update – United States Grand Prix 2021',
+      },
+      {
+        category: 'GAMING',
+        title: 'F1 Esports Pro Tips: Driving In The Wet',
+      },
+      {
+        category: 'F1',
+        title: 'Quiz: US Special',
+      },
+      {
+        category: 'F1',
+        title: 'Acura Returns For US Grand Prix',
+      },
+      {
+        category: 'F1',
+        title: 'United States Pre-Race Programme',
+      },
     ];
+
+    this.rxjsSubscriptions.push(
+      this.storyChangetimer.subscribe((_) => {
+        this.currentStory = (this.currentStory + 1) % this.totalStoryCount;
+      })
+    );
+  }
+
+  ngOnDestroy(): void {
+    // remove subscriptions
+    for (let subscription of this.rxjsSubscriptions) {
+      subscription.unsubscribe();
+    }
   }
 }
